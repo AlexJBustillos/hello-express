b001d0302717e63e6bcf04aed3fcd8a686f2926e
alexjbustillos <alexjbustillos@gmail.com>
2020-11-13 12:41:04 -0500

    [index]added about and blog

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function(req, res) {
     res.send('Hello, FROM THE MATRIX')
 });
 
+app.get('/about', function(req, res) {
+    res.send('This is about a page about pages about a page.');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Welcome to QUEENS!!');
+});
+
 app.listen(8000);
